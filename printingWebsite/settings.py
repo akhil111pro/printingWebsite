ddd7ece78a7905c0c20a964d2bfef91c5aaabd13
Akhil <akhilsubhash1@gmail.com>
2023-08-25 15:53:49 +0530

    Made changes and ready to be deployed

--- printingWebsite/settings.py
+++ printingWebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8*ss0h-xtcf%7j@iz2&p=d!3bl57n(p8jda2k3y!isw(4q0hz+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'django.contrib.sessions.middleware.SessionMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
