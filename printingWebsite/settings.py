dcf7804f3f8df64e5cf41f607152697f10371e65
Akhil <akhilsubhash1@gmail.com>
2023-08-25 16:48:15 +0530

    changed static

--- printingWebsite/settings.py
+++ printingWebsite/settings.py
@@ -120,7 +120,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "staticfiles/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , 'staticfiles')
+    os.path.join(BASE_DIR , 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
