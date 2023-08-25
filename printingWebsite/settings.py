5ebf3f2445aa26f0e0ab644446ab60d4b8e7fd93
Akhil <akhilsubhash1@gmail.com>
2023-08-25 16:23:43 +0530

    debug true

--- printingWebsite/settings.py
+++ printingWebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8*ss0h-xtcf%7j@iz2&p=d!3bl57n(p8jda2k3y!isw(4q0hz+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'django3dprinting.herokuapp.com']
 
